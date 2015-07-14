1ace1e2857354e6898cea995bb2c670aa08f0c0e
Benoit VALLON <benoitvallon@gmail.com>
2015-07-14 12:45:46 -0400

    Adds event listener to open edition

--- notepad.js
+++ notepad.js
@@ -12,14 +12,19 @@ goog.require('goog.dom');
 goog.require('goog.ui.Zippy');
 
 /**
- * Iterates over a list of note data objects, creates a Note instance
- * for each one, and tells the instance to build its DOM structure.
+ * Iterates over a list of note data objects, creates a
+ * tutorial.Note instance for each one, and tells the instance to build
+ * its DOM structure.
+ * @param {Array.<Object>} data The notes data.
+ * @param {Element} noteContainer The element under which DOM nodes for
+ *     the notes should be added.
+ * @return {Array.<tutorial.notepad.Note>} An array containing the resulting
+ *     instances.
  */
 tutorial.notepad.makeNotes = function(data, noteContainer) {
   var notes = [];
   for (var i = 0; i < data.length; i++) {
-    var note =
-      new tutorial.notepad.Note(data[i].title, data[i].content, noteContainer);
+    var note = new tutorial.notepad.Note(data[i], noteContainer);
     notes.push(note);
     note.makeNoteDom();
   }
@@ -28,10 +33,14 @@ tutorial.notepad.makeNotes = function(data, noteContainer) {
 
 /**
  * Manages the data and interface for a single note.
+ * @param {Array.<Object>} data The data for a single note.
+ * @param {Element} noteContainer The element under which DOM nodes for
+ *     the notes should be added.
+ * @constructor
  */
-tutorial.notepad.Note = function(title, content, noteContainer) {
-  this.title = title;
-  this.content = content;
+tutorial.notepad.Note = function(data, noteContainer) {
+  this.title = data.title;
+  this.content = data.content;
   this.parent = noteContainer;
 };
 
@@ -40,13 +49,47 @@ tutorial.notepad.Note = function(title, content, noteContainer) {
  */
 tutorial.notepad.Note.prototype.makeNoteDom = function() {
   // Create DOM structure to represent the note.
-  this.headerElement = goog.dom.createDom('div',
-      {'style': 'background-color:#EEE'}, this.title);
+  this.headerElement = goog.dom.createDom('div', null, this.title);
   this.contentElement = goog.dom.createDom('div', null, this.content);
+
+  // Create the editor text area and save button.
+  this.editorElement = goog.dom.createDom('textarea');
+  var saveBtn = goog.dom.createDom('input',
+      {'type': 'button', 'value': 'Save'});
+  this.editorContainer = goog.dom.createDom('div', {'style': 'display:none;'},
+      this.editorElement, saveBtn);
+
+  this.contentContainer = goog.dom.createDom('div', null,
+      this.contentElement, this.editorContainer);
+
+  // Wrap the editor and the content div in a single parent so they can
+  // be toggled in unison.
   var newNote = goog.dom.createDom('div', null,
-      this.headerElement, this.contentElement);
+      this.headerElement, this.contentContainer);
 
   // Add the note's DOM structure to the document.
-  goog.dom.appendChild(this.parent, newNote);
-  return new goog.ui.Zippy(this.headerElement, this.contentElement);
+  this.parent.appendChild(newNote);
+
+  // Attach the event handler that opens the editor.
+  goog.events.listen(this.contentElement, goog.events.EventType.CLICK,
+      this.openEditor);
+
+  // Attach the Zippy behavior.
+  this.zippy = new goog.ui.Zippy(this.headerElement, this.contentContainer);
+};
+
+/**
+ * Event handler for clicks on the content element. Clicking on the
+ * content element opens the editor field.
+ * @param {goog.events.Event} e The event object.
+ */
+tutorial.notepad.Note.prototype.openEditor = function(e) {
+  var elt = e.target;
+  var content = goog.dom.getTextContent(elt);
+  var editorContainer = goog.dom.getNextElementSibling(elt);
+  var editor = goog.dom.getFirstElementChild(editorContainer);
+
+  editor.innerHTML = content;
+  elt.style.display = 'none';
+  editorContainer.style.display = 'inline';
 };
